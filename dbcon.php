de8084fa2cf79e63477b63e514aa940c3c71237b
leckey <82240343+LeckeyDev@users.noreply.github.com>
2021-04-10 03:59:22 +0100

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -2,9 +2,9 @@
 function OpenCon()
  {
  $dbhost = "localhost";
- $dbuser = "lewdcc_idk";
- $dbpass = "Cartof1243$";
- $db = "lewdcc_pastr";
+ $dbuser = "root";
+ $dbpass = "yourpassword";
+ $db = "pastx";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
